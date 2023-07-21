ce86857e4b655a16822f994490cba635752d1b00
Victor Enhörning <victoren@axis.com>
2023-07-21 13:49:26 +0200

    MVP

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { Gif } from './gif/gif-component';
+import { Gif, GIF_ID, PAUSED_CLASS, PLAYING_CLASS } from './gif/gif.component';
+
+const PLAYING_TITLE = "slurp, slurp"
+const PAUSED_TITLE = "go ahead, take a sip"
+
+function Pause(setTitleState:React.Dispatch<React.SetStateAction<string>>){
+  const gifElement = document.getElementById(GIF_ID)
+  if(gifElement){
+    gifElement.classList.remove(PLAYING_CLASS);
+    gifElement.classList.add(PAUSED_CLASS);
+    setTitleState(PAUSED_TITLE)
+  }
+  else{
+    console.error(`Unable to find an element with ${GIF_ID}`)
+  }
+}
+
+function Play(setTitleState:React.Dispatch<React.SetStateAction<string>>){
+  const gifElement = document.getElementById(GIF_ID)
+  if(gifElement){
+    gifElement.classList.remove(PAUSED_CLASS);
+    gifElement.classList.add(PLAYING_CLASS);
+    setTitleState(PLAYING_TITLE)
+  }
+  else{
+    console.error(`Unable to find an element with ID: ${GIF_ID}`)
+  }
+}
 
 function App() {
+  const [titleState, setTitleState] = useState(PAUSED_TITLE)
   return (
     <div className="App">
-      <Gif/>
-      <button style={{backgroundColor: '#d2e7d6'}}>Start sipin</button>
-      <button style={{backgroundColor: '#fdaaaa'}}>Stop sipin</button>
+      <div id='center-div'>
+        <p id='title'>{titleState}</p>
+        <Gif/>
+        <button onClick={() => Play(setTitleState)} style={{backgroundColor: '#73B2D9'}}>👅💧</button>
+        <button onClick={() => Pause(setTitleState)} style={{backgroundColor: '#fdaaaa'}}>👅❌</button>
+      </div>
     </div>
   );
 }
--- src/gif/gif-component.tsx
+++
@@ -1,8 +0,0 @@
-export function Gif(){
-    return(
-        <div style={{width: '600', margin: 'auto', textAlign: 'center', fontFamily: 'ariel'}}>
-            <p >Go ahead, take a sip</p> 
-            <img src='https://media.giphy.com/media/Byana3FscAMGQ/giphy.gif' className='gif' />
-        </div>
-    )
-}
