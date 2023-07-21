8684b1813daf702f405cd1d45bcce2a5e1e61d4c
Victor Enhörning <victoren@axis.com>
2023-07-21 09:45:06 +0200

    Added gif and buttons

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,14 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Gif } from './gif/gif-component';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Gif/>
+      <button>Take a sip</button>
+      <button>Stop taking a sip</button>
     </div>
   );
 }
---
+++ src/gif/gif-component.tsx
@@ -0,0 +1,8 @@
+export function Gif(){
+    return(
+        <div style={{width: '600', margin: 'auto', textAlign: 'center', fontFamily: 'ariel'}}>
+            <p >Go ahead, take a sip</p> 
+            <img src='https://media.giphy.com/media/Byana3FscAMGQ/giphy.gif' className='gif' />
+        </div>
+    )
+}
