3e34bfc9597ded7b66f516d09b70fdd70a3b144f
Victor Enhörning <victoren@axis.com>
2023-07-21 10:25:58 +0200

    Added images for animation, styled buttons

--- src/App.tsx
+++ src/App.tsx
@@ -7,8 +7,8 @@ function App() {
   return (
     <div className="App">
       <Gif/>
-      <button>Take a sip</button>
-      <button>Stop taking a sip</button>
+      <button style={{backgroundColor: '#d2e7d6'}}>Start sipin</button>
+      <button style={{backgroundColor: '#fdaaaa'}}>Stop sipin</button>
     </div>
   );
 }
