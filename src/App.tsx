9aea902c23673664b935dc4dc2c91814660f120c
Victor Enhörning <victoren@axis.com>
2023-08-04 10:16:41 +0200

    Added prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,44 +1,57 @@
-import React, { useState } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import { Gif, GIF_ID, PAUSED_CLASS, PLAYING_CLASS } from './gif/gif.component';
+import React, { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import {
+  Gif,
+  GIF_ID,
+  PAUSED_CLASS,
+  PLAYING_CLASS,
+} from "./component/gif.component";
 
-const PLAYING_TITLE = "slurp, slurp"
-const PAUSED_TITLE = "go ahead, take a sip"
+const PLAYING_TITLE = "slurp, slurp";
+const PAUSED_TITLE = "go ahead, take a sip";
 
-function Pause(setTitleState:React.Dispatch<React.SetStateAction<string>>){
-  const gifElement = document.getElementById(GIF_ID)
-  if(gifElement){
+function Pause(setTitleState: React.Dispatch<React.SetStateAction<string>>) {
+  const gifElement = document.getElementById(GIF_ID);
+  if (gifElement) {
     gifElement.classList.remove(PLAYING_CLASS);
     gifElement.classList.add(PAUSED_CLASS);
-    setTitleState(PAUSED_TITLE)
-  }
-  else{
-    console.error(`Unable to find an element with ${GIF_ID}`)
+    setTitleState(PAUSED_TITLE);
+  } else {
+    console.error(`Unable to find an element with ${GIF_ID}`);
   }
 }
 
-function Play(setTitleState:React.Dispatch<React.SetStateAction<string>>){
-  const gifElement = document.getElementById(GIF_ID)
-  if(gifElement){
+function Play(setTitleState: React.Dispatch<React.SetStateAction<string>>) {
+  const gifElement = document.getElementById(GIF_ID);
+  if (gifElement) {
     gifElement.classList.remove(PAUSED_CLASS);
     gifElement.classList.add(PLAYING_CLASS);
-    setTitleState(PLAYING_TITLE)
-  }
-  else{
-    console.error(`Unable to find an element with ID: ${GIF_ID}`)
+    setTitleState(PLAYING_TITLE);
+  } else {
+    console.error(`Unable to find an element with ID: ${GIF_ID}`);
   }
 }
 
 function App() {
-  const [titleState, setTitleState] = useState(PAUSED_TITLE)
+  const [titleState, setTitleState] = useState(PAUSED_TITLE);
   return (
     <div className="App">
-      <div id='center-div'>
-        <p id='title'>{titleState}</p>
-        <Gif/>
-        <button onClick={() => Play(setTitleState)} style={{backgroundColor: '#73B2D9'}}>👅💧</button>
-        <button onClick={() => Pause(setTitleState)} style={{backgroundColor: '#fdaaaa'}}>👅❌</button>
+      <div id="center-div">
+        <p id="title">{titleState}</p>
+        <Gif />
+        <button
+          onClick={() => Play(setTitleState)}
+          style={{ backgroundColor: "#73B2D9" }}
+        >
+          👅💧
+        </button>
+        <button
+          onClick={() => Pause(setTitleState)}
+          style={{ backgroundColor: "#fdaaaa" }}
+        >
+          👅❌
+        </button>
       </div>
     </div>
   );
---
+++ src/component/gif.component.tsx
@@ -0,0 +1,9 @@
+import "./gif.css";
+
+export const GIF_ID = "gif";
+export const PLAYING_CLASS = "playing";
+export const PAUSED_CLASS = "paused";
+
+export function Gif() {
+  return <div id={GIF_ID} className={PAUSED_CLASS} />;
+}
