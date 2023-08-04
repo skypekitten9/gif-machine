484030c5ea8c88cb35c37cf184ca59bdecdb64e7
Victor Enhörning <victoren@axis.com>
2023-08-04 13:20:55 +0200

    Animations now controlled by state machine

--- src/App.tsx
+++ src/App.tsx
@@ -1,67 +1,26 @@
 import React, { useState } from "react";
-import logo from "./logo.svg";
 import "./App.css";
-import {
-  Gif,
-  GIF_ID,
-  PAUSED_CLASS,
-  PLAYING_CLASS,
-} from "./component/gif.component";
+import { Gif } from "./component/gif.component";
 import { useMachine } from "@xstate/react";
 import { gifMachine } from "./machine/gifMachine";
-import { stat } from "fs";
 
-const PLAYING_TITLE = "slurp, slurp";
-const PAUSED_TITLE = "go ahead, take a sip";
 type SendType = ReturnType<typeof useMachine>[1];
 
-function Pause(
-  setTitleState: React.Dispatch<React.SetStateAction<string>>,
-  send: SendType,
-) {
-  send("PAUSE");
-  const gifElement = document.getElementById(GIF_ID);
-  if (gifElement) {
-    gifElement.classList.remove(PLAYING_CLASS);
-    gifElement.classList.add(PAUSED_CLASS);
-    setTitleState(PAUSED_TITLE);
-  } else {
-    console.error(`Unable to find an element with ${GIF_ID}`);
-  }
-}
-
-function Play(
-  setTitleState: React.Dispatch<React.SetStateAction<string>>,
-  send: SendType,
-) {
-  send("PLAY");
-  const gifElement = document.getElementById(GIF_ID);
-  if (gifElement) {
-    gifElement.classList.remove(PAUSED_CLASS);
-    gifElement.classList.add(PLAYING_CLASS);
-    setTitleState(PLAYING_TITLE);
-  } else {
-    console.error(`Unable to find an element with ID: ${GIF_ID}`);
-  }
-}
-
 function App() {
-  const [titleState, setTitleState] = useState(PAUSED_TITLE);
   const [state, send] = useMachine(gifMachine);
   console.log("State: ", state.value);
   return (
     <div className="App">
       <div id="center-div">
-        <p id="title">{titleState}</p>
         <Gif />
         <button
-          onClick={() => Play(setTitleState, send)}
+          onClick={() => send("PLAY")}
           style={{ backgroundColor: "#73B2D9" }}
         >
           👅💧
         </button>
         <button
-          onClick={() => Pause(setTitleState, send)}
+          onClick={() => send("PAUSE")}
           style={{ backgroundColor: "#fdaaaa" }}
         >
           👅❌
--- src/machine/gifMachine.ts
+++ src/machine/gifMachine.ts
@@ -1,27 +1,59 @@
 import { createMachine } from "xstate";
+import {
+  GIF_ID,
+  PAUSED_CLASS,
+  PLAYING_CLASS,
+} from "../component/gif.component";
 
-export const gifMachine = createMachine({
-  /** @xstate-layout N4IgpgJg5mDOIC5RQJYDMCyBDAxgCxQDswA6ABywFdZIBiABQBkBBATQG0AGAXUVDID2sFABcUAwnxAAPRAEYATAHYSANgCsq5ZwAs6hVoUAOADQgAnvM0lO6uZp0BmJXICcO1ascBfb2dSYuATE5AA2WOZEUAzMAKoAygCiXLxIIILCYhJSsgj2OmpGTkruDgq6ZpYIRnIkOkZK6pyqTgquqkqcRr5+IIQCEHBSAdj4RGBSGaLikmm5ALSqlYjz6iSuG5tbWz69I0Hj5FQ0EJNC09lziDoKy9UqmroKN0aO7R06vv7oo8GkZOFIoQoGdMjMcogDKoSEZmgp1EYjC0OuodDo7q5ODZVE8Xm9PEpPj0gA */
-  id: "gifMachine",
-  schema: {
-    events: {} as { type: "PLAY" } | { type: "PAUSE" },
-  },
-  initial: "paused",
-  states: {
-    paused: {
-      on: {
-        PLAY: {
-          target: "playing",
+export const gifMachine = createMachine(
+  {
+    /** @xstate-layout N4IgpgJg5mDOIC5RQJYDMCyBDAxgCxQDswA6ABywFdZIBiABQBkBBATQG0AGAXUVDID2sFABcUAwnxAAPRAEYATAHYSANgCsq5ZwAs6hVoUAOADQgAnvM0lO6uZp0BmJXICcO1ascBfb2dSYuATE5AA2WOZEUAzMAKoAygCiXLxIIILCYhJSsgj2OmpGTkruDgq6ZpYIRnIkOkZK6pyqTgquqkqcRr5+IIQCEHBSAdj4RGBSGaLikmm5ALSqlYjz6iSuG5tbWz69I0Hj5FQ0EJNC09lziDoKy9UqmroKN0aO7R06vv7oo8GkZOFIoQoGdMjMcogDKoSEZmgp1EYjC0OuodDo7q5ODZVE8Xm9PEpPj0gA */
+    id: "gifMachine",
+    schema: {
+      events: {} as { type: "PLAY" } | { type: "PAUSE" },
+    },
+    initial: "paused",
+    states: {
+      paused: {
+        on: {
+          PLAY: {
+            target: "playing",
+            actions: "playAction",
+          },
+        },
+      },
+
+      playing: {
+        on: {
+          PAUSE: {
+            target: "paused",
+            actions: "pauseAction",
+          },
         },
       },
     },
-
-    playing: {
-      on: {
-        PAUSE: {
-          target: "paused",
-        },
+  },
+  {
+    actions: {
+      playAction: () => {
+        const gifElement = document.getElementById(GIF_ID);
+        if (gifElement) {
+          gifElement.classList.remove(PAUSED_CLASS);
+          gifElement.classList.add(PLAYING_CLASS);
+        } else {
+          console.error(`Unable to find an element with ID: ${GIF_ID}`);
+        }
+      },
+      pauseAction: () => {
+        console.log("Pause action!");
+        const gifElement = document.getElementById(GIF_ID);
+        if (gifElement) {
+          gifElement.classList.remove(PLAYING_CLASS);
+          gifElement.classList.add(PAUSED_CLASS);
+        } else {
+          console.error(`Unable to find an element with ${GIF_ID}`);
+        }
       },
     },
   },
-});
+);
